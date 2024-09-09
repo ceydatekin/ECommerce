8c88b1a32d5547a0adcd920ba7950a21d2234b65
ceyda <ceydatekn85@gmail.com>
2024-09-09 23:12:35 +0300

    add swagger configurations

--- src/Services/ProductService/ECommerce.ProductService.API/Program.cs
+++ src/Services/ProductService/ECommerce.ProductService.API/Program.cs
@@ -1,3 +1,5 @@
+using MongoDB.Driver;
+
 namespace ECommerce.ProductService.API;
 
 public class Program
@@ -6,25 +8,32 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
         builder.Services.AddAuthorization();
 
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        
+        var mongoConnectionString = builder.Configuration.GetConnectionString("MongoDb");
 
+        builder.Services.AddSingleton<IMongoClient>(new MongoClient(mongoConnectionString));
+
+        builder.Services.AddScoped<IMongoDatabase>(sp =>
+        {
+            var client = sp.GetRequiredService<IMongoClient>();
+            return client.GetDatabase("Product");
+        });
+        builder.Services.AddControllers();
+        
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
 
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
+        // API endpoint'lerini tanımlayın
+        app.MapControllers(); 
 
         app.Run();
     }
